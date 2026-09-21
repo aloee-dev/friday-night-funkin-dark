e9a7b89b8670aff331c98879d5ff0fdb9049d6e7
aloee-dev <gfeng@bostonk12.org>
2026-09-21 18:45:23 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'ACE-engine-v0.0.0';
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
