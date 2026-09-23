fa20b93aa204a57c6c8dbb922fb30cc08dbf3f3e
aloee-dev <gfeng@bostonk12.org>
2026-09-23 13:28:50 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,22 +10,27 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
     if (!event.request.url.startsWith(self.location.origin)) return;
 
-    if (event.request.url.includes('?preload=true')) {
-        return;
+    let targetUrl = event.request.url;
+
+    if (targetUrl.includes('?preload=true')) {
+        targetUrl = targetUrl.replace('?preload=true', '');
     }
 
-    const isAudio = event.request.url.match(/\.(mp3|m4a|wav)$/i);
-    const requestToProcess = isAudio ? new Request(event.request.url) : event.request;
+    const isAudio = targetUrl.match(/\.(mp3|m4a|wav)$/i);
+    const requestToProcess = new Request(targetUrl, isAudio ? {} : event.request);
 
     event.respondWith(
         caches.match(requestToProcess).then((cachedResponse) => {
             if (cachedResponse) return cachedResponse;
 
-            return fetch(requestToProcess).then((networkResponse) => {
+            return fetch(event.request).then((networkResponse) => {
                 if (!networkResponse || networkResponse.status !== 200) return networkResponse;
 
                 const responseToCache = networkResponse.clone();
-                caches.open(CACHE_NAME).then((cache) => cache.put(requestToProcess, responseToCache));
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(requestToProcess, responseToCache);
+                });
+
                 return networkResponse;
             });
         })
